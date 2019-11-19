2fa7d0c72b9ca8e3c1b61b14f77aedac559b276b
ramakms29 <ramakms29@gmail.com>
2019-11-19 19:31:46 +0530

    Initial Project setup with maven dependencies,...etc.

---
+++ src/test/java/com/raki/animals/AppTest.java
@@ -0,0 +1,12 @@
+package com.raki.animals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testMain() throws Exception {
+        throw new RuntimeException("not yet implemented");
+    }
+
+}
